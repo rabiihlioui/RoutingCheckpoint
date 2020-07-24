8ed5e5cc10130ed095c9c569fe1e66d5582a6e9a
rabiihlioui <rabiihlioui@gmail.com>
2020-07-24 13:49:47 +0100

    Routing Checkpoint Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# RoutingCheckpoint
+# StructuralDirectivesNgForCheckpoint
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.3.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CvComponent } from './cv/cv.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+import { ItemComponent } from './item/item.component';
+import { HiredCvComponent } from './hired-cv/hired-cv.component';
+import { HiredCvItemComponent } from './hired-cv-item/hired-cv-item.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CvDetailsComponent } from './cv-details/cv-details.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ListComponent,
+    DetailComponent,
+    ItemComponent,
+    HiredCvComponent,
+    HiredCvItemComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    CvDetailsComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
